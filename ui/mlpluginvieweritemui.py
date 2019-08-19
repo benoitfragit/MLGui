ad21988b245c079c16e181413b1f88f7bcc6add3
Benoit Franquet <benoitfranquet@gmail.com>
2019-08-19 22:48:02 +0200

    Adding new periodic timer into the MLTrainerViewerItem, starting the MLPlginViewerItem

---
+++ ui/mlpluginvieweritemui.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QCheckBox
+from PyQt5.QtCore    import pyqtSignal
+
+import uuid
+
+class MLPluginViewerItemUI(QWidget):
+
+    activatePlugin      = pyqtSignal(uuid.UUID)
+    deactivatePlugin    = pyqtSignal(uuid.UUID)
+
+    def __init__(self, plugin, parent = None):
+        QWidget.__init__(self, parent)
+
+        self._plugin = plugin
+
+        hbox1 = QHBoxLayout()
+        hbox2 = QHBoxLayout()
+        hbox3 = QHBoxLayout()
+        hbox4 = QHBoxLayout()
+
+        vbox = QVBoxLayout()
+
+        self._check = QCheckBox()
+        self._check.setChecked(self._plugin.mlIsPluginActivated())
+        self._check.toggled.connect(self.mlOnPluginActivationToggled)
+        hbox1.addWidget(QLabel('Plugin'))
+        hbox1.addWidget(QLabel(self._plugin.mlGetPluginName()))
+        hbox1.addStretch(1)
+        hbox1.addWidget(self._check)
+
+        hbox2.addWidget(QLabel('Author'))
+        hbox2.addStretch(1)
+        hbox2.addWidget(QLabel(self._plugin.mlGetPluginAuthor()))
+
+        hbox3.addWidget(QLabel('Version'))
+        hbox3.addStretch(1)
+        hbox3.addWidget(QLabel(self._plugin.mlGetPluginVersion()))
+
+        hbox4.addWidget(QLabel('Description'))
+        hbox4.addStretch(1)
+        hbox4.addWidget(QLabel(self._plugin.mlGetPluginDescription()))
+
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        vbox.addLayout(hbox4)
+
+        self.setLayout(vbox)
+
+    def mlOnPluginActivationToggled(self):
+        if self._check.isChecked():
+            self._plugin.mlSetPluginActivated(True)
+            self.activatePlugin.emit(self._plugin.mlGetUniqId())
+        else:
+            self._plugin.mlSetPluginActivated(False)
+            self.deactivatePlugin.emit(self._plugin.mlGetUniqId())
